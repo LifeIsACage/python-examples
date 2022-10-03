fb9c248c7ee02180a80ccda2024aa72bdd82f067
LifeIsACage <67647191+LifeIsACage@users.noreply.github.com>
2022-10-03 19:49:32 +0200

    Add files via upload

---
+++ basic/square.py
@@ -0,0 +1,9 @@
+size = int(input("Insert square size: "))
+
+#print("*" *size) Empty square
+
+for _ in range(size-2):
+    print("*"*size) #filled square
+    #print("*", " "*(size-4), "*") Empty square
+
+#print("*" * size) Empty square
